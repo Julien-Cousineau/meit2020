efe8d5b63ad775c5900539ba84e792f71fe18309
Julien Cousineau <julien.cousineau@gmail.com>
2020-12-18 11:10:25 -0500

    Update example.py

--- api/example.py
+++ api/example.py
@@ -1,8 +1,7 @@
+import pandas as pd
 from pymapd import connect
-con = connect(user="publicuser", password="password",protocol="https", host="ocremap.ca",port=9092,dbname="meit")
+con = connect(user="publicuser", password="password",protocol="https", host="ec-meit.ca",port="/api",dbname="meit")
 
 # mapmeit
-str1 = "SELECT mapmeit as key0,SUM(nox*1) AS nox FROM t2015a WHERE nox*1 IS NOT NULL GROUP BY key0 ORDER BY nox DESC LIMIT 50"
-
-df = con.select_ipc(str1)
-print(df)
+sql = "SELECT mapmeit as key0,SUM(nox*1) AS nox FROM DB_2015 WHERE nox IS NOT NULL GROUP BY key0 ORDER BY nox DESC LIMIT 50"
+pd.read_sql(sql,con).to_csv("example.csv",index=False)
